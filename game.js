dbd9b7e9f7a4b6303646a64f0ebedd5dd897b050
EshwarAnad <71984010+EshwarAnad@users.noreply.github.com>
2020-11-09 14:12:20 +0530

    push

---
+++ game.js
@@ -0,0 +1,4 @@
+//Here is the link for the bulb when the button is turn on.
+//https://image.flaticon.com/icons/svg/702/702797.svg
+document.getElementById("turnon").onclick=()=>{document.getElementById("image1").src="https://image.flaticon.com/icons/svg/702/702797.svg"};
+document.getElementById("turnoff").onclick=()=>{document.getElementById("image1").src="https://image.flaticon.com/icons/svg/702/702814.svg"};
